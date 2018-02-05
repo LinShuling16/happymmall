cdb0efd3bec1b66c7a7a40b075e877d9a94fef24
linshuling <1611074393@qq.com>
2018-02-05 12:14:34 +0800

    webpack 配置

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,7 +2,7 @@
 * @Author: linshuling
 * @Date:   2018-02-02 14:24:18
 * @Last Modified by:   linshuling
-* @Last Modified time: 2018-02-02 17:38:52
+* @Last Modified time: 2018-02-05 11:51:01
 */
 'use strict';
 require('./index.css');
